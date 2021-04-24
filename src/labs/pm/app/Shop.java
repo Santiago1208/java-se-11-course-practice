5c48c96f24e509526a6c888bead6edfc71c143ac
santiago-restrepo <sanpra1208@gmail.com>
2021-04-24 17:55:27 -0500

    create the Product Management Application
    Shop and Product classes, create new product, print it.

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021 Santiago
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import labs.pm.data.Product;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Shop {
+    
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        p1.setId(10);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+        
+        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice());
+    }
+    
+}
